e9a12e002172dc7076fe6a11fd1f26d9e207b91d
Ivan <ivan007244@gmail.com>
2019-12-28 04:13:40 +0700

    Убран лишний код

--- bot.js
+++ bot.js
@@ -33,6 +33,7 @@ fileToExport.forEach((pluginPart) => {
     let command = require(pluginPart);
 
     console.log(`[] Loading '${pluginPart}' plugin with ${command.length} command(s)`)
+    
     try {
         helpers.loadCommand(command, (modulePart) => { 
             updates.hear(modulePart.hear, (context) => modulePart.execute(context, vk))
@@ -46,47 +47,6 @@ fileToExport.forEach((pluginPart) => {
 })
 
 
-/*let hearList, onList, useList;
-
-if (fs.existsSync("./modules/hear")) hearList = fs.readdirSync("./modules/hear")
-if (fs.existsSync("./modules/on")) onList = fs.readdirSync("./modules/on")
-if (fs.existsSync("./modules/use")) useList = fs.readdirSync("./modules/use")
-
-
-let readModule = (fname) => {
-    if (!fname.endsWith(".js")) return undefined;
-    let ret = require(fname)
-    console.log(`[i]\t[+] Loading '${fname.split("/")[3].replace(".js", "")}' module with ${ret.length} handlers`)
-    return ret;
-}
-
-let parseModule = (list, dir, fun) => {
-    if(list != undefined) {
-        for(let moduleFile of list) {
-            let module = readModule(`./modules/${dir}/${moduleFile}`)
-            if(module != undefined) {
-                console.log("Module: ", module)
-                module.forEach((modulePart) => {
-                    console.log("Module part: ", modulePart)
-                    fun(modulePart)
-                })
-            }
-        }
-    }
-}
-
-
-parseModule(hearList, "hear", (modulePart) => { 
-    updates.hear(modulePart.hear, (context) => modulePart.execute(context, vk)) 
-})
-parseModule(onList, "on", (modulePart) => { 
-    updates.on(modulePart.on, (context) => modulePart.execute(context, vk)) 
-})
-parseModule(useList, "use", (modulePart) => { 
-    updates.use(modulePart.use, (context) => modulePart.execute(context, vk)) 
-})*/
-
-
 updates.setHearFallbackHandler(async (context) => {
     if (context.isChat) return;
     await context.send(`Введите /help для получения помощи`);
@@ -96,4 +56,3 @@ updates.setHearFallbackHandler(async (context) => {
 updates.startPolling().then((r)=>{
     console.log("[] Start hearing LongPoll");
 }).catch(console.error);
-
