6e634bbadb1385389aaebf34bf3f6bd365df599f
Ivan <ivan007244@gmail.com>
2019-12-28 04:03:11 +0700

    Переписан код загрузки команд

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const { VK } = require('vk-io');
 const fs = require('fs');
 const config = require('./config.json');
 const utils = require('./utils.js');
+const helpers = require('./helpers/loader.js')
 
 
 const vk = new VK ({
@@ -13,9 +14,39 @@ const vk = new VK ({
 const { updates } = vk;
 
 
-console.log("[i] Modules initialization...");
+console.log("[] Initing plugins...");
 
-let hearList, onList, useList;
+let pluginsFolder = fs.readdirSync("./plugins/hear");
+let fileToExport = [];
+
+for (let item of pluginsFolder) {
+    if (item.endsWith(".js")) {
+        fileToExport.push(`./plugins/hear/${item}`);
+        continue;
+    }
+}
+
+console.log("[] Load plugins...");
+
+fileToExport.forEach((pluginPart) => {
+
+    let command = require(pluginPart);
+
+    console.log(`[] Loading '${pluginPart}' plugin with ${command.length} command(s)`)
+    try {
+        helpers.loadCommand(command, (modulePart) => { 
+            updates.hear(modulePart.hear, (context) => modulePart.execute(context, vk))
+        })
+
+    } catch (e) {
+        console.log("Error occurred while loading plugin commands!");
+        console.log(pluginPath);
+        console.log(e);
+    } 
+})
+
+
+/*let hearList, onList, useList;
 
 if (fs.existsSync("./modules/hear")) hearList = fs.readdirSync("./modules/hear")
 if (fs.existsSync("./modules/on")) onList = fs.readdirSync("./modules/on")
@@ -34,7 +65,9 @@ let parseModule = (list, dir, fun) => {
         for(let moduleFile of list) {
             let module = readModule(`./modules/${dir}/${moduleFile}`)
             if(module != undefined) {
+                console.log("Module: ", module)
                 module.forEach((modulePart) => {
+                    console.log("Module part: ", modulePart)
                     fun(modulePart)
                 })
             }
@@ -51,15 +84,16 @@ parseModule(onList, "on", (modulePart) => {
 })
 parseModule(useList, "use", (modulePart) => { 
     updates.use(modulePart.use, (context) => modulePart.execute(context, vk)) 
-})
+})*/
 
 
 updates.setHearFallbackHandler(async (context) => {
     if (context.isChat) return;
-    await context.send(`${config.is_explicit ? 'Введите /help для получения помощи' : 'Такой команды нет. Введите /help для получения помощи'}`);
+    await context.send(`Введите /help для получения помощи`);
 });
 
-console.log("[i] All modules successfully loaded. Starting bot...")
 
+updates.startPolling().then((r)=>{
+    console.log("[] Start hearing LongPoll");
+}).catch(console.error);
 
-updates.startPolling().then(console.log(`[i] Bot successfully started :)`)).catch(console.error);
