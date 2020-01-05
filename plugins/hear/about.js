fc0d9026fb8032db03f78feea2ebd628a3085a17
Ivan <ivan007244@gmail.com>
2020-01-05 18:23:46 +0700

    Редактирвоание плагинов
    -Замена пробелов на табы

--- plugins/hear/about.js
+++ plugins/hear/about.js
@@ -2,19 +2,19 @@ const info = require('../../package.json');
 const utils = require('../../utils.js');
 
 const about = async(context, vk) => {
-    await context.send(`
-    ${info.name} - ${info.description}
+	await context.send(`
+	${info.name} - ${info.description}
 
-    Версия: ${info.version}
-    Git: ${utils.getGitCommitHash(false)}
-    Разработчик: @rejson (Иван) | @es3n1n (Арсений)
-    Платформа: Node JS ${info.engines.node}.
-    Используемая библиотека: vk-io`);
+	Версия: ${info.version}
+	Git: ${utils.getGitCommitHash(false)}
+	Разработчик: @rejson (Иван) | @es3n1n (Арсений)
+	Платформа: Node JS ${info.engines.node}.
+	Используемая библиотека: vk-io`);
 }
 
 module.exports = [
-    {
-        hear: '/about',
-        execute: about
-    }
+	{
+		hear: '/about',
+		execute: about
+	}
 ]
