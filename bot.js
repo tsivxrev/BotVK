f71f92d89ec1153251cc22e8aa91c01accd56c7d
Ivan <ivan007244@gmail.com>
2020-01-10 15:15:10 +0700

    И это тоже.

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const { VK } = require('vk-io');
 
 const fs = require('fs');
 const config = require('./config.json');
-const utils = require('./utils.js');
 const helpers = require('./helpers/loader.js')
 
 
