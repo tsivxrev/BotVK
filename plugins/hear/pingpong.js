2cedcfa9c6e7fb179f979f69dc701aab73652358
Ivan <ivan007244@gmail.com>
2020-01-10 15:31:27 +0700

    Почему бы и да?

--- plugins/hear/pingpong.js
+++ plugins/hear/pingpong.js
@@ -2,10 +2,18 @@ const pingPong = async (context, next) => {
 	await context.send(`pong`);
 }
 
+const mInfo = async (context) => {
+	await context.reply(JSON.stringify(context, null, '\t'));
+}
+
 
 module.exports = [
 	{
 		hear: [/^(пинг|ping)$/i, "/ping"],
 		execute: pingPong
+	}, 
+	{
+		hear: '/minfo',
+		execute: mInfo
 	}
 ];
