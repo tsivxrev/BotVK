69cfbc94ff953d48d8d40971d43604cd71e24f7f
Ivan <ivan007244@gmail.com>
2020-01-14 16:46:33 +0700

    Убраны лишние импорты в  stat.js

--- plugins/hear/stat.js
+++ plugins/hear/stat.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
 const utils = require('../../utils.js');
-const config = require('../../config.json');
+
 
 const uptime = async (context, vk) => {
     await context.send(`Uptime: ${utils.uptime()}`);
 }
+
 
 module.exports = [
     {
