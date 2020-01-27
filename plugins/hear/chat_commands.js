84c4f0a24768dc169f7432c3a353dd54f5bca40a
Ivan <ivan007244@gmail.com>
2020-01-28 03:32:36 +0700

    ¯\_(ツ)_/¯

--- plugins/hear/chat_commands.js
+++ plugins/hear/chat_commands.js
@@ -1,6 +1,10 @@
 const mInfo = async (context) => {
     context.loadMessagePayload()
-	await context.reply(JSON.stringify(context, null, '\t'));
+    
+    let msgId = context.id;
+    let msgInfo = await vk.api.call('messages.getById', {'message_ids': msgId});
+
+    await context.reply(JSON.stringify(msgInfo, null, '\t'));
 }
 
 const shrug = async (context ,vk) => {
