a6815c2e4d34ceebbbb273c6abd5cc0420f7fa18
Ivan <ivan007244@gmail.com>
2020-01-31 03:53:50 +0700

    Update about.js

--- plugins/hear/about.js
+++ plugins/hear/about.js
@@ -2,8 +2,6 @@ const info = require('../../package.json');
 const utils = require('../../utils.js');
 
 
-/* ubi rerum testimonia adsunt quid opus est verbis */
-
 const about = async(context, vk) => {
 	await context.send(`
 	${info.name} - ${info.description}
